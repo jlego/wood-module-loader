221fea07c3c69d18ad7ac99cbb0d321134e3d0d1
jlego <273734893@qq.com>
2018-11-18 01:21:49 +0800

    no message

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Wood Plugin Module.
+ * 模块自动加载
+ * by jlego on 2018-11-18
+ */
+ const path = require('path');
+ const fs = require('fs');
+
+module.exports = (app, config = {}) => {
+  ['model', 'controller', 'route'].forEach(type => {
+    let dirPath = config.registerDirs[type];
+    const dirList = fs.readdirSync(path.resolve(__dirname, dirPath));
+    dirList.forEach((fileName) => {
+      let nameArr = fileName.split('.'),
+        moduleName = nameArr[0],
+        fileExt = nameArr[1];
+      if (fileExt === 'js') {
+        let theModule = require(path.resolve(__dirname, `${dirPath}/${moduleName}`));
+        if (type === 'controller') {
+          let controllerName = moduleName.replace('Controller', '');
+          if(!app._controllers.has(controllerName)){
+            theModule = typeof theModule === 'function' ? new theModule({}, _models) : theModule;
+            app._controllers.set(controllerName, theModule);
+          }
+        }
+      }
+    });
+  });
+  return app;
+}
